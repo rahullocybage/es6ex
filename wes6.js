58bff9e342fa40e7502a3fe840b479c3abeb5657
rahullocybage <rahullocybage.com>
2022-12-27 13:30:39 +0530

    first commit

---
+++ wes6.js
@@ -0,0 +1,20 @@
+var add =function(a,b){
+    this.a = a,
+    this.b = b,
+    this.addi = function() {
+    console.log(this);
+    
+    setTimeout(() =>{
+        console.log(this.a+this.b);
+    },3000);
+}
+}
+
+let add1 = new add(34,54);
+add1.addi();
+//     addition :function() {
+
+//         return "The addition of "+this.a+" and "+this.b+" is "+(this.a +this.b);
+//     }
+// };
+// console.log(add.addition());
